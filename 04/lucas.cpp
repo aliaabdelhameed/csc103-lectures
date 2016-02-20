89657b03c07497f96ff664c6f63b6580a3e35907
Jane Chen <jchen014@citymail.cuny.edu>
2016-02-20 04:48:04 -0500

    did TODOs

--- 04/lucas.cpp
+++ 04/lucas.cpp
@@ -11,39 +11,32 @@ int main()
 	/* Lucas sequences (of the first kind):
 	 * U_n(P,Q) = P*U_{n-1}(P,Q) - Q*U_{n-2}(P,Q).
 	 * */
-	
-	/* first read parameters P and Q: */
-	cout << "Enter P and Q: ";
-	int P,Q;
-	cin >> P;
-	cin >> Q;
-	cout << "enter terms to compute: ";
-	int n;
-	while (cin >> n) {
-		/* compute nth term of sequence. */
-		if (n == 0) {
-			cout << 0 << "\n";
-		} else if (n == 1) {
-			cout << 1 << "\n";
-		} else {
-			int a0 = 0; /* term two before current */
-			int a1 = 1; /* term one before current */
-			/* use n as a counter: */
-			while (n > 1) {
-				/* compute next term; update a0 and a1
-				 * to maintain their meaning */
-				int next = P*a1 - Q*a0;
-				a0 = a1;
-				a1 = next;
-				n--;
-			}
-			cout << a1 << "\n";
-		}
+
 		/* TODO: delete this whole thing and write it from scratch. */
 		/* TODO: while you're at it, simplify this.
 		 * You don't need the "else if" block. */
-	}
 
+	
+	int P,Q;
+	cout << "Enter P and Q: ";
+	cin >> P >> Q;
+	cout << "Enter number of terms to compute: ";
+	int n;
+	while (cin >> n) {
+		if (n == 0 || n == 1) {
+			cout << n << endl;
+		} else {
+			int num1 = 0;
+			int num2 = 1;
+			while (n > 1) {
+				int temp = num2 * P - num1 * Q;
+				num1 = num2;
+				num2 = temp;
+				n--;
+			}
+			cout << num2 << endl;
+		}
+	}
 	return 0;
 }
 
